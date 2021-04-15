7606170aff045bb9608c5c496f9341ebb58705a2
unknown <samangharat@gmail.com>
2021-04-15 17:38:35 +0100

    Add a simple broad phase for wall collisions

--- RougeLike/Source/Collisions/WallCollisionTracker.cpp
+++ RougeLike/Source/Collisions/WallCollisionTracker.cpp
@@ -5,80 +5,118 @@
 #include "Actors/Actor.h"
 
 
-VectorF WallCollisionTracker::allowedVelocity(const Map* map, VectorF velocity, float dt) const
+VectorF WallCollisionTracker::allowedVelocity(const Map* map, VectorF velocity, float dt) 
 {
-	// extra buffer to stop small movemets getting stuck in the wall?
-	float bufferFactor = 3.0f;
-	velocity *= bufferFactor;
-
-	restrictLeftMovement(map, velocity, dt);
-	restrictRightMovement(map, velocity, dt);
-	restrictDownMovement(map, velocity, dt);
-	restrictTopMovement(map, velocity, dt);
-
-	return velocity / bufferFactor;
-}
-
-
-void WallCollisionTracker::restrictLeftMovement(const Map* map, VectorF& movement, float dt) const
-{
-	if (movement.x < 0.0f)
+	if (braodPhaseCollision(map, velocity, dt))
 	{
-		RectF rect = wallScaledRect();
-		rect = rect.Translate(VectorF(movement.x, 0.0f) * dt);
+		mWallScaledRect = wallScaledRect();
 
-		const MapTile* topLeft = map->tile(rect.TopLeft());
-		const MapTile* bottomLeft = map->tile(rect.BotLeft());
+		// Increase the movement to make sure we dont get stuck in a wall or something
+		float bufferFactor = 2.0f;
+		velocity *= bufferFactor;
 
-		if (cannotMove(topLeft) || cannotMove(bottomLeft))
-			movement.x = 0.0f;
+		if (velocity.x > 0.0f)
+		{
+			restrictRightMovement(map, velocity, dt);
+		}
+		else if (velocity.x < 0.0f)
+		{
+			restrictLeftMovement(map, velocity, dt);
+		}
+
+		if (velocity.y > 0.0f)
+		{
+			restrictDownMovement(map, velocity, dt);
+		}
+		else if (velocity.y < 0.0f)
+		{
+			restrictUpMovement(map, velocity, dt);
+		}
+
+		return velocity / bufferFactor;
+	}
+	else // no collision so we dont touch the velocity
+	{
+		return velocity;
 	}
 }
 
-void WallCollisionTracker::restrictRightMovement(const Map* map, VectorF& movement, float dt) const
+
+bool WallCollisionTracker::braodPhaseCollision(const Map* map, VectorF& velocity, float dt) const
 {
-	if (movement.x > 0.0f)
+	// Create a rect with size = size + movement and check if any corners are on a wall tile
+	RectF rect = wallScaledRect();
+	VectorF center = rect.Center();
+	VectorF size = rect.Size() + velocity * 2.0f * dt;
+	rect.SetSize(size);
+	rect.SetCenter(center);
+
+	const MapTile* tiles[4] = { map->tile(rect.TopLeft()), map->tile(rect.TopRight()), map->tile(rect.BotRight()), map->tile(rect.BotLeft()) };
+	for (int i = 0; i < 4; i++)
 	{
-		RectF rect = wallScaledRect();
-		rect = rect.Translate(VectorF(movement.x, 0.0f) * dt);
-
-		const MapTile* topRight = map->tile(rect.TopRight());
-		const MapTile* bottomRight = map->tile(rect.BotRight());
-
-		if (cannotMove(topRight) || cannotMove(bottomRight))
-			movement.x = 0.0f;
+		if (tiles[i] && tiles[i]->is(CollisionTile::Floor))
+		{
+			return true;
+		}
 	}
+
+	return false;
 }
 
-void WallCollisionTracker::restrictDownMovement(const Map* map, VectorF& movement, float dt) const
+
+void WallCollisionTracker::restrictUpMovement(const Map* map, VectorF& velocity, float dt) const
 {
-	if (movement.y > 0.0f)
-	{
-		RectF rect = wallScaledRect();
-		rect = rect.Translate(VectorF(0.0f, movement.y) * dt);
+	RectF rect = mWallScaledRect;
+	rect = rect.Translate(VectorF(0.0f, velocity.y) * dt);
 
-		const MapTile* bottomRight = map->tile(rect.BotRight());
-		const MapTile* bottomLeft = map->tile(rect.BotLeft());
+	const MapTile* topLeft = map->tile(rect.TopLeft());
+	const MapTile* topRight = map->tile(rect.TopRight());
 
-		if (cannotMove(bottomRight) || cannotMove(bottomLeft))
-			movement.y = 0.0f;
-	}
+	if (cannotMove(topLeft) || cannotMove(topRight))
+		velocity.y = 0.0f;
 }
 
-void WallCollisionTracker::restrictTopMovement(const Map* map, VectorF& movement, float dt) const
+
+void WallCollisionTracker::restrictRightMovement(const Map* map, VectorF& velocity, float dt) const
 {
-	if (movement.y < 0.0f)
-	{
-		RectF rect = wallScaledRect();
-		rect = rect.Translate(VectorF(0.0f, movement.y) * dt);
+	RectF rect = mWallScaledRect;
+	rect = rect.Translate(VectorF(velocity.x, 0.0f) * dt);
 
-		const MapTile* topLeft = map->tile(rect.TopLeft());
-		const MapTile* topRight = map->tile(rect.TopRight());
+	const MapTile* topRight = map->tile(rect.TopRight());
+	const MapTile* bottomRight = map->tile(rect.BotRight());
 
-		if (cannotMove(topLeft) || cannotMove(topRight))
-			movement.y = 0.0f;
-	}
+	if (cannotMove(topRight) || cannotMove(bottomRight))
+		velocity.x = 0.0f;
 }
+
+
+
+void WallCollisionTracker::restrictDownMovement(const Map* map, VectorF& velocity, float dt) const
+{
+	RectF rect = mWallScaledRect;
+	rect = rect.Translate(VectorF(0.0f, velocity.y) * dt);
+
+	const MapTile* bottomRight = map->tile(rect.BotRight());
+	const MapTile* bottomLeft = map->tile(rect.BotLeft());
+
+	if (cannotMove(bottomRight) || cannotMove(bottomLeft))
+		velocity.y = 0.0f;
+}
+
+
+
+void WallCollisionTracker::restrictLeftMovement(const Map* map, VectorF& velocity, float dt) const
+{
+	RectF rect = mWallScaledRect;
+	rect = rect.Translate(VectorF(velocity.x, 0.0f) * dt);
+
+	const MapTile* topLeft = map->tile(rect.TopLeft());
+	const MapTile* bottomLeft = map->tile(rect.BotLeft());
+
+	if (cannotMove(topLeft) || cannotMove(bottomLeft))
+		velocity.x = 0.0f;
+}
+
 
 
 RectF WallCollisionTracker::wallScaledRect() const
--- RougeLike/Source/Collisions/WallCollisionTracker.h
+++ RougeLike/Source/Collisions/WallCollisionTracker.h
@@ -13,22 +13,25 @@ public:
 	void setActor(const Actor* actor) { mActor = actor; mRect = nullptr; }
 	void setRect(const RectF* rect) { mRect = rect; mActor = nullptr; }
 
-	VectorF allowedVelocity(const Map* map, VectorF velocity, float dt) const;
+	VectorF allowedVelocity(const Map* map, VectorF velocity, float dt);
 
 
 private:
+	bool braodPhaseCollision(const Map* map, VectorF& movement, float dt) const;
+
 	void restrictLeftMovement(const Map* map, VectorF& movement, float dt) const;
 	void restrictRightMovement(const Map* map, VectorF& movement, float dt) const;
-	void restrictTopMovement(const Map* map, VectorF& movement, float dt) const;
+	void restrictUpMovement(const Map* map, VectorF& movement, float dt) const;
 	void restrictDownMovement(const Map* map, VectorF& movement, float dt) const;
 
 	RectF wallScaledRect() const;
 
 	bool cannotMove(const MapTile* tile) const;
 
-
 private:
 	const RectF* mRect;
 	const Actor* mActor;
+
+	RectF mWallScaledRect;
 };
 
